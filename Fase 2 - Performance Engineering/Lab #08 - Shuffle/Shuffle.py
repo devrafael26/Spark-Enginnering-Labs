4a8d21bbfd62f3f8ffa7482e979d95066d17d2e2
Rafael Diniz <105223642+devrafael26@users.noreply.github.com>
2026-08-29 11:17:54 -0300

    Add files via upload

---
+++ Fase 2 - Performance Engineering/Lab #08 - Shuffle/Shuffle.py
@@ -0,0 +1,48 @@
+from pyspark.sql import functions as F
+
+df_orders = (
+    spark.range(5_000_000)
+    .withColumn("customer_id", (F.col("id") % 100_000).cast("long"))
+    .withColumn("category_id", (F.col("id") % 20).cast("int"))
+    .withColumn("amount", (F.rand() * 1000).cast("double"))
+)
+
+df_result5m = (
+    df_orders
+    .groupBy("category_id")
+    .agg(
+        F.count("*").alias("total_orders"),
+        F.sum("amount").alias("total_amount")
+    )
+)
+
+df_result5m.show()
+
+df_result5m.explain(True)
+
+
+# Alterando o range para 10 milhões de registros
+
+from pyspark.sql import functions as F
+
+df_orders10m = (
+    spark.range(10_000_000)
+    .withColumn("customer_id", (F.col("id") % 100_000).cast("long"))
+    .withColumn("category_id", (F.col("id") % 20).cast("int"))
+    .withColumn("amount", (F.rand() * 1000).cast("double"))
+)
+
+
+df_result10m = (
+    df_orders10m
+    .groupBy("category_id")
+    .agg(
+        F.count("*").alias("total_orders"),
+        F.sum("amount").alias("total_amount")
+    )
+)
+
+df_result10m.explain(True)
+
+
+spark.conf.get("spark.sql.shuffle.partitions")
